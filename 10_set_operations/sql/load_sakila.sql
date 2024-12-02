f39cf619e74963ceffa0fafaa7edd68b1ffba2ce
IrinaAntipina <iramaskaykina@gmail.com>
2024-12-02 12:47:04 +0100

    set operations

---
+++ 10_set_operations/sql/load_sakila.sql
@@ -0,0 +1,5 @@
+INSTALL sqlite;
+LOAD sqlite;
+
+-- absolute path to sqlite-sakile.db
+CALL sqlite_attach('/Users/ira/Documents/DE24/github/sql-irina-antipina-de24/10_set_operations/data/sqlite-sakila.db')
